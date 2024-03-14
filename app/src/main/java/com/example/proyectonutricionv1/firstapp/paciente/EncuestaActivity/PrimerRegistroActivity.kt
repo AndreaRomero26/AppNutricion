b54cec320272944aa6c0df398be27b0bac6c9a27
AndreaRomero26 <a337438@uach.mx>
2024-03-13 22:17:48 -0600

    Se agrego deteccion de campos completos en primer registro

--- app/src/main/java/com/example/proyectonutricionv1/firstapp/paciente/EncuestaActivity/PrimerRegistroActivity.kt
+++ app/src/main/java/com/example/proyectonutricionv1/firstapp/paciente/EncuestaActivity/PrimerRegistroActivity.kt
@@ -3,6 +3,8 @@ package com.example.proyectonutricionv1.firstapp.paciente.EncuestaActivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -23,6 +25,8 @@ class PrimerRegistroActivity : AppCompatActivity() {
     private lateinit var editText6: EditText
     private lateinit var editText7: EditText
     private lateinit var editText8: EditText
+    private lateinit var btn_sig_encuesta: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,15 +42,40 @@ class PrimerRegistroActivity : AppCompatActivity() {
         editText8 = findViewById(R.id.editTextLocalidad)
 
         val textViewFecha = findViewById<TextView>(R.id.respuesta_fecha)
-
-        val btn_sig_encuesta = findViewById<Button>(R.id.btn_sig_encuesta)
-
+        btn_sig_encuesta = findViewById<Button>(R.id.btn_sig_encuesta)
         val intent_sig_encuesta = Intent(this, EncuestaActivity::class.java)
 
         val currentDate = Calendar.getInstance().time
         val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
         val formattedDate = dateFormat.format(currentDate)
         textViewFecha.setText(formattedDate)
+
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // No se utiliza en este caso
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // No se utiliza en este caso
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                checkEditTexts()
+            }
+        }
+
+        editText1.addTextChangedListener(textWatcher)
+        editText2.addTextChangedListener(textWatcher)
+        editText3.addTextChangedListener(textWatcher)
+        editText4.addTextChangedListener(textWatcher)
+        editText5.addTextChangedListener(textWatcher)
+        editText6.addTextChangedListener(textWatcher)
+        editText7.addTextChangedListener(textWatcher)
+        editText8.addTextChangedListener(textWatcher)
+
+        // Inicialmente, deshabilitar el botón
+        btn_sig_encuesta.isEnabled = false
 
         btn_sig_encuesta.setOnClickListener {
             val value1 = editText7.text.toString()
@@ -67,7 +96,29 @@ class PrimerRegistroActivity : AppCompatActivity() {
             intent_sig_encuesta.putExtra("COC", value8)
             startActivity(intent_sig_encuesta)
         }
+    }
 
+    private fun checkEditTexts() {
+        val nombreProf = editText1.text.toString()
+        val primerApellido = editText2.text.toString()
+        val segundoApellido = editText3.text.toString()
+        val nombres = editText4.text.toString()
+        val fechaNac = editText5.text.toString()
+        val perimetro = editText6.text.toString()
+        val municipio = editText7.text.toString()
+        val localidad = editText8.text.toString()
 
+        // Verificar si todos los EditText tienen texto
+        val allFilled = nombreProf.isNotEmpty() &&
+                primerApellido.isNotEmpty() &&
+                segundoApellido.isNotEmpty() &&
+                nombres.isNotEmpty() &&
+                fechaNac.isNotEmpty() &&
+                perimetro.isNotEmpty() &&
+                municipio.isNotEmpty() &&
+                localidad.isNotEmpty()
+
+        // Habilitar o deshabilitar el botón según si todos los EditText tienen texto
+        btn_sig_encuesta.isEnabled = allFilled
     }
 }
