8ea2c0cfe125ba35881d6310fb377ba7fe4e1923
AndreaRomero26 <a337438@uach.mx>
2024-03-27 20:09:02 -0600

    Seleccionar px en mostrar progreso

---
+++ app/src/main/java/com/example/proyectonutricionv1/firstapp/paciente/seguimiento/MostrarP2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.proyectonutricionv1.firstapp.paciente.seguimiento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyectonutricionv1.R
+
+class MostrarP2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_p2)
+    }
+}
