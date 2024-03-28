57907f62c6cc6fe3cefe30d5003efc642b26f001
AndreaRomero26 <a337438@uach.mx>
2024-03-27 20:44:49 -0600

    Avance de editar expediantes (primer pantalla con DB y seleccion de folio)

---
+++ app/src/main/java/com/example/proyectonutricionv1/firstapp/staff/Editar2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.proyectonutricionv1.firstapp.staff
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyectonutricionv1.R
+
+class Editar2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar2)
+    }
+}
